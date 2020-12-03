53e2e77d4e94e476fadbc1bebf67652de2928058
Blarc <jakob.malezic@gmail.com>
2020-12-03 19:21:13 +0100

    added consul discovery and cleaned up poms

--- src/main/java/rso/prediction/PredictionApplication.java
+++ src/main/java/rso/prediction/PredictionApplication.java
@@ -4,15 +4,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.web.client.RestTemplate;
 
-import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
-
 @SpringBootApplication
+@EnableFeignClients
 public class PredictionApplication {
 
     private static final Logger log = LoggerFactory.getLogger(PredictionApplication.class);
@@ -33,8 +33,8 @@ public class PredictionApplication {
         log.info("Connected to postgres: " + dbUrl);
     }
 
-    @Bean
-    public Hibernate5Module hibernate5Module() {
-        return new Hibernate5Module();
-    }
+//    @Bean
+//    public Hibernate5Module hibernate5Module() {
+//        return new Hibernate5Module();
+//    }
 }
