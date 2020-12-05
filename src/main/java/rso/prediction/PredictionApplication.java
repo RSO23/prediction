c784e2c7375d9f451ea68b8c3a3a1d2805102cc7
Blarc <jakob.malezic@gmail.com>
2020-12-05 16:10:39 +0100

    fixed consul config

--- src/main/java/rso/prediction/PredictionApplication.java
+++ src/main/java/rso/prediction/PredictionApplication.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -11,8 +12,9 @@ import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
 @EnableFeignClients
+@SpringBootApplication
+@ConfigurationPropertiesScan
 public class PredictionApplication {
 
     private static final Logger log = LoggerFactory.getLogger(PredictionApplication.class);
--- src/main/java/rso/prediction/controller/PredictionController.java
+++ src/main/java/rso/prediction/controller/PredictionController.java
@@ -2,27 +2,31 @@ package rso.prediction.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+import rso.prediction.config.ConfigProperties;
 import rso.prediction.entity.Prediction;
 import rso.prediction.service.PredictionService;
 
 @RestController
+@RequiredArgsConstructor
 public class PredictionController {
 
-    private final PredictionService predictionService;
+    private final ConfigProperties configProperties;
 
-    @Autowired
-    public PredictionController(PredictionService predictionService){
-        this.predictionService = predictionService;
-    }
+    private final PredictionService predictionService;
 
     @GetMapping("/ping")
     public String ping(){
         return "Pong!";
+    }
+
+    @GetMapping("/config")
+    public String testConfig() {
+        return configProperties.getTestConfig();
     }
 
     @GetMapping("/predictions/{gameId}")
