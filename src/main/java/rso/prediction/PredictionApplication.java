45d51edd218958327b561b0a91243798a6ba135d
Blarc <jakob.malezic@gmail.com>
2020-12-08 18:30:15 +0100

    added swagger and changed prod context path to /prediction

--- src/main/java/rso/prediction/PredictionApplication.java
+++ src/main/java/rso/prediction/PredictionApplication.java
@@ -1,5 +1,8 @@
 package rso.prediction;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
@@ -11,11 +14,24 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.paths.DefaultPathProvider;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableFeignClients
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableSwagger2
 public class PredictionApplication {
 
     private static final Logger log = LoggerFactory.getLogger(PredictionApplication.class);
@@ -35,6 +51,39 @@ public class PredictionApplication {
         String dbUrl = applicationContext.getEnvironment().getProperty("spring.datasource.url");
         log.info("Connected to postgres: " + dbUrl);
         MDC.put("applicationName", applicationContext.getId());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public Docket swaggerConfig() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("rso.prediction"))
+                .paths(PathSelectors.any())
+                .build()
+                .securitySchemes(List.of(apiKey()))
+                .apiInfo(apiDetails());
+    }
+
+    private ApiInfo apiDetails() {
+        return new ApiInfo(
+                "Prediction API",
+                "This is a private API for League of Legends predictor application",
+                "1.0",
+                "Students license",
+                new Contact("Jakob Maležič", "https://github.com/Blarc", "jm6421@student.uni-lj.si"),
+                "API License",
+                "https://google.com",
+                Collections.emptyList()
+        );
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
     }
 
 //    @Bean
--- src/main/java/rso/prediction/controller/PredictionController.java
+++ src/main/java/rso/prediction/controller/PredictionController.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import rso.prediction.config.ConfigProperties;
 import rso.prediction.entity.Prediction;
@@ -15,6 +18,8 @@ import rso.prediction.service.PredictionService;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+@Api(value = "Basic operations", produces = "application/json", consumes = "application/json")
 public class PredictionController {
 
     private final Logger log = LoggerFactory.getLogger(PredictionController.class);
