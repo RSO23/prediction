b3363231e9ee3c660f185d5206335ca59b8d8ca9
Blarc <jakob.malezic@gmail.com>
2021-01-07 21:42:39 +0100

    added custom health check, fixed LogContextInterceptor, fixed logit

--- src/main/java/rso/prediction/controller/PredictionController.java
+++ src/main/java/rso/prediction/controller/PredictionController.java
@@ -2,7 +2,9 @@ package rso.prediction.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
+import rso.prediction.component.CustomHealthCheck;
 import rso.prediction.config.ConfigProperties;
 import rso.prediction.dto.MatchDto;
 import rso.prediction.dto.PredictionDto;
@@ -26,10 +29,11 @@ public class PredictionController {
 
     private final Logger log = LoggerFactory.getLogger(PredictionController.class);
 
-
     private final ConfigProperties configProperties;
 
     private final PredictionService predictionService;
+
+    private final CustomHealthCheck customHealthCheck;
 
     @GetMapping("/ping")
     public String ping(){
@@ -51,5 +55,12 @@ public class PredictionController {
     public PredictionDto makePredictionSummoners(@RequestBody SummonerNamesDto summonerNamesDto) {
         return predictionService.makePredictionsSummoners(summonerNamesDto);
     }
+
+    @PostMapping("/break")
+    public ResponseEntity<Void> breakPod() {
+        customHealthCheck.setState("DOWN");
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 
 }
